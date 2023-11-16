5f43277230019ce6433fe695143627db15dfe9bf
aruan4 <aruan4@uwo.ca>
2023-11-15 23:30:38 -0500

    edited projects appearance

--- src/components/newProjects.jsx
+++ src/components/newProjects.jsx
@@ -5,8 +5,12 @@ var newProjects = () => {
     return(
         <div className='flex justify-center items-center'>
             <ul className='w-[60%]'>
-                <li className='border-[#f72828] border-2 rounded-lg'>
+                <li className='relative border-[#f72828] border-2 rounded-lg'>
                     <img src={Songify} alt='Kekw' className=''></img>
+                    <div className='absolute top-[50%] left-[50%] w-[50%] p-4 translate-y-[-50%] bg-[#492b49]'>
+                        <p className='text-white'>Songify uses CoHere API and Spotify API to add songs to the user's
+                        your playlist using any prompt the user wants.</p>
+                    </div>
                 </li>
             </ul>
         </div>
