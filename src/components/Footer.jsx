ca7a30a60b421c66e397a2b7b17482ab3f297f4d
aruan4 <114169630+aruan4@users.noreply.github.com>
2023-11-16 16:17:47 -0500

    added footer links and hover effects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     FaGithubSquare,
     FaInstagramSquare,
@@ -7,16 +7,42 @@ import {
 } from 'react-icons/fa';
 
 const Footer = () => {
+    const [gitHover, setGit] = useState(false); 
+    const [fbHover, setFB] = useState(false);
+    const [igHover, setIG] = useState(false);
+    const [liHover, setLI] = useState(false);
+
+    const handleGit = () => {
+        setGit(!gitHover);
+    }
+    const handleFB = () => {
+        setFB(!fbHover);
+    }
+    const handleIG = () => {
+        setIG(!igHover);
+    }
+    const handleLI = () => {
+        setLI(!liHover);
+    } 
+
     return(
         <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
             <div>
-                <h1 className='w-full text-3xl font-bold text-[#fdcfff]'>anson.</h1>
+                <h1 className='w-full text-3xl font-bold text-[#5e91aa]'>anson.</h1>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, est nesciunt officiis assumenda sequi, earum ad quidem ipsam libero vitae expedita magni eos. Totam eius animi dolorem saepe fugiat consectetur?</p>
                 <div className='flex justify-between md:w-[50%] my-6'>
-                    <FaGithubSquare size={30}/>
-                    <FaFacebookSquare size={30}/>
-                    <FaInstagramSquare size={30}/>
-                    <FaLinkedin size={30}/>
+                    <a href='https://github.com/ansonruan7?tab=repositories' onMouseEnter={handleGit} onMouseLeave={handleGit}>
+                        {!gitHover ? <FaGithubSquare size={30}/> : <FaGithubSquare size={30} color={'grey'}/>}
+                    </a>
+                    <a href='https://www.facebook.com/anson.ruan.96/' onMouseEnter={handleFB} onMouseLeave={handleFB}>
+                        {!fbHover ? <FaFacebookSquare size={30}/> : <FaFacebookSquare size={30} color={'grey'}/>}
+                    </a>                                       
+                    <a href='https://www.instagram.com/notansonruan/' onMouseEnter={handleIG} onMouseLeave={handleIG}>
+                        {!igHover ? <FaInstagramSquare size={30}/> : <FaInstagramSquare size={30} color={'grey'}/>}
+                    </a>                        
+                    <a href='https://www.linkedin.com/in/anson-ruan-0b137520b/' onMouseEnter={handleLI} onMouseLeave={handleLI}>
+                        {!liHover ? <FaLinkedin size={30}/> : <FaLinkedin size={30} color={'grey'}/>}
+                    </a>                        
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = function(){
 }
     return(
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-            <h1 className='w-full text-3xl font-bold text-[#d8d8d8]'>anson.</h1>
+            <h1 className='w-full text-3xl font-bold text-[#5e91aa]'>anson.</h1>
             <ul className='hidden md:flex'>
                 <li className='p-4'>Home</li>
                 <li className='p-4'>About</li>
@@ -21,7 +21,7 @@ const Navbar = function(){
                 {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             </div>
             <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                <h1 className='w-full text-3xl font-bold text-[#d8d8d8] m-4'>anson.</h1>
+                <h1 className='w-full text-3xl font-bold text-[#5e91aa] m-4'>anson.</h1>
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-gray-600'>Home</li>
                     <li className='p-4 border-b border-gray-600'>About</li>
