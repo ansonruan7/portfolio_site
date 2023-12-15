7a4fb0bd60f1a9dabdffb1bbb6b226ccafa19380
aruan4 <aruan4@uwo.ca>
2023-12-15 18:03:01 -0500

    added new project

--- src/components/newProjects.jsx
+++ src/components/newProjects.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Songify from '../assets/Songify.jpg';
 import BaadDays from '../assets/BaadDays.jpg';
+import superheroDB from '../assets/superheroDB.png';
 import {FaGithubSquare} from 'react-icons/fa';
 
 
@@ -55,6 +56,26 @@ var NewProjects = () => {
                         </div>
                         <div className='absolute lg:bottom-16 lg:left-[20%] sm:bottom-0 bottom-2' onMouseEnter={handleHover} onMouseLeave={handleHover}>
                             <a href='https://github.com/aruan4/Baad-Day'>
+                                {!isHovered ? <FaGithubSquare size={30} color={'#5e91aa'}/> : <FaGithubSquare size={30} color={'#6ca5c2'}/>}
+                            </a>
+                        </div>
+                    </li>
+                    {/*Right Side*/}
+                    <li className='relative rounded-lg px-6 md:my-6 my-12'>
+                        <img src={superheroDB} alt='Kekw' className='sm:scale-75 lg:translate-x-[-12.5%] bg-[#251625]/[75%] lg:opacity-50 opacity-25'></img>
+                        <div className='lg:absolute top-[50%] lg:left-[50%] lg:w-[50%] translate-y-[-50%] p-4 
+                        lg:bg-[#492b49] bg-[#492b49]/50  drop-shadow-md hover:bg-[#583458]
+                        sm:text-sm text-xs
+                        flex justify-center items-center'>
+                            <div className='absolute sm:top-[-75%] lg:right-[10%] top-[-60%]'>
+                                <p className='text-[#5e91aa] text-lg font-bold'>SuperheroDB</p>
+                                <p className='text-[#d8d8d8] text-sm'>Western University</p>
+                            </div>
+                            <p className='text-white bg-vil'>SuperheroDB uses React.js, Node.js, Express, Firebase Auth, and Firestore to deliver
+                            superhero information to the user.</p>
+                        </div>
+                        <div className='absolute lg:bottom-16 lg:right-[20%] sm:bottom-0 bottom-2' onMouseEnter={handleHover} onMouseLeave={handleHover}>
+                            <a href='https://github.com/aruan4/se3316-aruan4-lab4'>
                                 {!isHovered ? <FaGithubSquare size={30} color={'#5e91aa'}/> : <FaGithubSquare size={30} color={'#6ca5c2'}/>}
                             </a>
                         </div>
